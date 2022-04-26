c11af94333bd23e8aecf56a2d89f993098db10be
zhihao <1348261663@qq.com>
2022-04-26 11:29:51 +0800

    添加icon、iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,29 @@
 <template>
-    <button class="g-button">button2</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#icon-erp-cross-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
 export default {
-    
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: "left"
+        }
+    }
 }
 </script>
 <style lang="scss">
 .g-button {
+    display: inline-flex;
+    align-items: center;
+    vertical-align: middle;
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
@@ -22,6 +38,23 @@ export default {
     }
     &:focus {
         outline: none;
+    }
+    > .icon {
+        order: 1;
+        margin-right: .1em;
+    }
+    > .content {
+        order: 2;
+    }
+    &.icon-right {
+        > .icon {
+            order: 2;
+            margin-right: 0;
+            margin-left: .1em;
+        }
+        > .content {
+            order: 1;
+        }
     }
 }
 </style>
