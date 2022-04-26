8ffc602e52a7a943ac53c70c27ad1b80b70c16f0
zhihao <1348261663@qq.com>
2022-04-26 19:58:53 +0800

    将svg封装到icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg class="g-icon" aria-hidden="true">
+        <use :xlink:href="`#icon-erp-cross-${name}`"></use>
+    </svg>
+</template>
+<script>
+export default {
+    props: {
+        name: String
+    }
+}
+</script>
+<style lang="scss">
+.g-icon {
+    width: 1em;
+    height: 1em;
+}
+</style>
