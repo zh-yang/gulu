96002973938b372f068809aa5307b6791cc4bed7
zhihao <1348261663@qq.com>
2022-05-01 09:53:14 +0800

    更新karma依赖

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dis/**/*.test.js',
+      'dist/**/*.test.js',
       'dist/**/*.test.css'
     ],
 
