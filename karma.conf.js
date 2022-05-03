1ba01ee9e914eb1b434b094344cdfc2726af5f52
zhihao <1348261663@qq.com>
2022-05-03 10:12:24 +0800

    更新karma配置

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'sinon-chai'],
+    client: {
+      chai: {
+        includeStack: true
+      }
+    },
 
 
     // list of files / patterns to load in the browser
