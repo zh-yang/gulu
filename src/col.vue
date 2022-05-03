9370f4a7fb35726d966e092c492f9ca1d1382cc2
zhihao <1348261663@qq.com>
2022-05-03 16:20:46 +0800

    初始化row、col

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+</div>
+</template>
+<script>
+export default {
+    name: "GuluCol",
+    props: {
+        span: [Number, String]
+    }
+}
+</script>
+<style lang="scss" scoped>
+.col {
+    height: 32px;
+    &:nth-child(even) {
+        background: gray;
+    }
+    &:nth-child(odd) {
+        background: pink;
+    }
+    $class: col-;
+    @for $n from 1 through 24 {
+        &.#{$class}#{$n} {
+            width: $n/24 * 100%;
+        }
+    }
+}
+</style>
